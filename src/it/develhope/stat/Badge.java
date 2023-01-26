9bf9423b777e7a1fdd89ef54e1e6ae3f96fc9f68
Giulia <giuliac35@gmail.com>
2023-01-26 18:27:36 +0100

    Add attributes to Badge class, add private method keepTrackOfEmployeesNumber

--- src/it/develhope/stat/Badge.java
+++ src/it/develhope/stat/Badge.java
@@ -1,4 +1,11 @@
 package it.develhope.stat;
 
 public class Badge {
+    private static int totalNumberOfEmployees;
+    private String badgeIdCode;
+    public Employee employee;
+
+    private static void keepTrackOfEmployeesNumber(){
+        ++totalNumberOfEmployees;
+    }
 }
