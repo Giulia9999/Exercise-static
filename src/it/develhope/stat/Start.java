36de53057465889c658c081309147bf9fabc6357
Giulia <giuliac35@gmail.com>
2023-01-26 20:06:17 +0100

    Create 2 Badge objects in Start clÃass

--- src/it/develhope/stat/Start.java
+++ src/it/develhope/stat/Start.java
@@ -4,5 +4,8 @@ public class Start {
     public static void main(String[] args) {
         Employee employee1 = new Employee("Luca", "Rossi", "Via Nino Bixio");
         Employee employee2 = new Employee("Giulia", "Contarino", "Via Giuseppe Gribaldi");
+
+        Badge employee1Badge = new Badge(employee1);
+        Badge employee2Badge = new Badge(employee2);
     }
 }
