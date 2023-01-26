7564d63435306071365b489e4d1aae5fc3ab6c9f
Giulia <giuliac35@gmail.com>
2023-01-26 18:20:43 +0100

    Create cEmployee constructor

--- src/it/develhope/stat/Employee.java
+++ src/it/develhope/stat/Employee.java
@@ -4,4 +4,10 @@ public class Employee {
     private String name;
     private String surname;
     private String address;
+
+    public Employee(String newEmployeeName, String newEmployeeSurname, String newEmployeeAddress){
+        this.name = newEmployeeName;
+        this.surname = newEmployeeSurname;
+        this.address = newEmployeeAddress;
+    }
 }
